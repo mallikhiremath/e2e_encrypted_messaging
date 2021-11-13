a8b3731da173fc63243b31fe5a1335f94e40f64c
Mallik Hiremath <Mallikarjunaiah.Hiremath@vanderbilt.edu>
2021-11-12 18:04:54 -0800

    With single channel chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  /// Create a new instance of [StreamChatClient] passing the apikey obtained from your
+  /// project dashboard.
+  final client = StreamChatClient(
+    'm7b7hqpfuke8',
+    logLevel: Level.INFO,
+  );
+
+  /// Set the current user. In a production scenario, this should be done using
+  /// a backend to generate a user token using our server SDK.
+  /// Please see the following for more information:
+  /// https://getstream.io/chat/docs/flutter-dart/tokens_and_authentication/?language=dart
+  await client.connectUser(
+    User(id: 'MallikH', name: 'Mallik Hiremath'),
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiTWFsbGlrSCJ9.jg_CD9SSg-7HaASlwDDe7rohep2is48p6Gjv5Q8VR14",
+  );
+
+  User user2 = User(id: 'SatyaS', name: 'Satya Salvi');
+
+  /// Creates a channel using the type `messaging` and `flutterdevs`.
+  /// Channels are containers for holding messages between different members. To
+  /// learn more about channels and some of our predefined types, checkout our
+  /// our channel docs: https://getstream.io/chat/docs/flutter-dart/creating_channels/?language=dart
+  final channel = client.channel(
+    'messaging',
+    id: 'food-channel',
+    extraData: {
+      "name": "Food Chat",
+      "image": "http://bit.ly/2O35mws",
+      "members": ["MallikH", "SatyaS"],
+    },
+  );
+
+  /// `.watch()` is used to create and listen to the channel for updates. If the
+  /// channel already exists, it will simply listen for new events.
+  await channel.watch();
+
+  runApp(
+    MyApp(
+      client: client,
+      channel: channel,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  /// To initialize this example, an instance of [client] and [channel] is required.
+  const MyApp({
+    Key? key,
+    required this.client,
+    required this.channel,
+  }) : super(key: key);
 
-  // This widget is the root of your application.
+  /// Instance of [StreamChatClient] we created earlier. This contains information about
+  /// our application and connection state.
+  final StreamChatClient client;
+
+  /// The channel we'd like to observe and participate.
+  final Channel channel;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      builder: (context, widget) {
+        return StreamChat(
+          client: client,
+          child: widget,
+        );
+      },
+      home: StreamChannel(
+        channel: channel,
+        child: const ChannelPage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+/// Displays the list of messages inside the channel
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: const ChannelHeader(),
+      body: Column(
+        children: const <Widget>[
+          Expanded(
+            child: MessageListView(),
+          ),
+          MessageInput(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
